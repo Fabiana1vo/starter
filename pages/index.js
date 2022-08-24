0b36ea92ef1e9545c7709e80e148d6cefee995b4
Fabiana Ivo <72579427+Fabiana1vo@users.noreply.github.com>
2022-08-24 17:32:52 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Olá!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Olá" />
+        <Header title="Site em construção" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Volte em breve
         </p>
       </main>
 
